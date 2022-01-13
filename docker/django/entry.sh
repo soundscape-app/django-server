31bfe8e48d30ea4b20f15ead6f1ff5a214eb76cb
ymjoo12 <ymjoo12@naver.com>
2022-01-13 16:54:09 +0900

    feat: Edit gunicorn config

--- docker/django/entry.sh
+++ docker/django/entry.sh
@@ -2,7 +2,11 @@
 
 # fab -f fabfile.py load_secrets
 python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate
+
+NUM_WORKERS=3
+TIMEOUT=120
 
 exec gunicorn backend.wsgi:application \
 --workers $NUM_WORKERS \
